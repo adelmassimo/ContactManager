393a3e8661bc44dacd49ece6561fe4fe63f5ce51
adelmassimo <adel.massimo.ramadan@gmail.com>
2017-11-21 17:15:47 +0100

    Initial Commit

---
+++ ContactManager/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ContactManager
+//
+//  Created by adel on 21/11/2017.
+//  Copyright © 2017 Adel Ramadan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ContactManager/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ContactManager
+//
+//  Created by adel on 21/11/2017.
+//  Copyright © 2017 Adel Ramadan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
